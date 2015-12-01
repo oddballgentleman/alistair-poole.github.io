9e8d189db6afa1e1acf4ae3c33dd4cfe765a43cf
Alistair <pooleali@msu.edu>
2015-12-01 15:04:14 -0500

    First commit

---
+++ showhide.js
@@ -0,0 +1,43 @@
+function showhide1()
+ {
+       var div = document.getElementById("type1");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
+ 
+function showhide2()
+ {
+       var div = document.getElementById("type2");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
+ 
+function showhide3()
+ {
+       var div = document.getElementById("type3");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
+ 
+function showhide4()
+ {
+       var div = document.getElementById("type4");
+if (div.style.display !== "none") {
+    div.style.display = "none";
+}
+else {
+    div.style.display = "block";
+}
+ }
